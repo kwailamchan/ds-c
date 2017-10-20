09f7989523f778378ec71fa20a8b55dd058ac258
K <kwailamchan@hotmail.com>
2017-10-20 14:28:32 +0800

    [master] 	modified:   ds/inc/stack.h 	modified:   ds/inc/tree.h 	modified:   ds/tests/test_tree.c

--- ds/tests/test_tree.c
+++ ds/tests/test_tree.c
@@ -7,12 +7,23 @@
 
 void test_tree (void)
 {
-  struct node * root = treenode_create(1);
+  struct treenode * root = treenode_create(1);
   root->left = treenode_create(2);
   root->right = treenode_create(3);
   root->left->left = treenode_create(4);
+  tree_insert (root, 5);
+  tree_insert (root, 6);
+  tree_delete (root, 6);
 
-  return 0;
+  printf("preorder: "); tree_traverse_preorder (root); printf("\n");
+  printf("inorder: "); tree_traverse_inorder (root); printf("\n");
+  printf("postorder: "); tree_traverse_postorder (root); printf("\n");
+  printf("levelorder: "); tree_traverse_levelorder (root); printf("\n");
+  printf("morris inorder: "); tree_traverse_morris (root); printf("\n");
+  printf("kdistance: "); tree_traverse_kdistance (root, 1); printf("\n");
+
+  int maxwidth = tree_maxwidth (root);
+  printf("maxwidth: %d\n", maxwidth);
 }
 
 
